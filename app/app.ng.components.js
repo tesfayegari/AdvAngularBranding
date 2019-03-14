eab09fe47ee2f09597c54e24583e0954860629ea
Tesfaye Gari <TGari@chemtrec.com>
2019-03-13 21:08:46 -0400

    Added some Angular Apps and Masterpage edited

---
+++ app/app.ng.components.js
@@ -0,0 +1,152 @@
+(function () {
+  'use strict';
+
+  mtmDemo.component('topNavigation', {
+    controllerAs: 'vm',
+    template: "<div ng-include='vm.tempUrl'></div>",
+    bindings: {
+      title: '@'
+    },
+    controller: function (svc) {
+      var vm = this;
+      vm.$onInit = function () {
+        // TODO: Initialization goes here
+      };
+      vm.tempUrl = svc.getTemplateUrl('topmenu.html');
+
+    }
+
+  });
+
+  mtmDemo.component('globalNavigation', {
+    controllerAs: 'vm',
+    template: "<div ng-include='vm.tempUrl'></div>",
+    bindings: {
+      title: '@'
+    },
+    controller: function (svc) {
+      var vm = this;
+      vm.$onInit = function () {
+        // TODO: Initialization goes here
+      };
+      vm.tempUrl = svc.getTemplateUrl('globalnav.html');
+
+    }
+
+  });
+
+  
+  mtmDemo.component('mtmFooter', {
+    controllerAs: 'vm',
+    template: "<div ng-include='vm.tempUrl'></div>",
+    bindings: {
+      title: '@'
+    },
+    controller: function (svc) {
+      var vm = this;
+      vm.$onInit = function () {
+        // TODO: Initialization goes here
+      };
+      vm.tempUrl = svc.getTemplateUrl('footer.html');
+
+    }
+
+  });
+
+
+  // mtmDemo.component('carosel', {
+  //   controllerAs: 'vm',
+  //   template: "<div ng-include='vm.tempUrl'></div>",
+  //   bindings: {
+  //     title: '@'
+  //   },
+  //   controller: function (svc) {
+  //     var vm = this;
+      
+  //     vm.$onInit = function () {
+  //       // TODO: Initialization goes here
+  //     };
+  //     vm.tempUrl = svc.getTemplateUrl('carosel.html');
+
+  //   }
+
+  // });
+
+
+  // mtmDemo.component('accordion', {
+  //   controllerAs: 'vm',
+  //   template: "<div ng-include='vm.tempUrl'></div>",
+  //   bindings: {
+  //     title: '@'
+  //   },
+  //   controller: function (svc) {
+  //     var vm = this;
+  //     vm.$onInit = function () {
+  //       var filter = '?$filter=Disable eq false';
+  //       svc.getItems("AccordionList", filter).then(function (results) {
+  //         vm.items = results;          
+  //         console.log(vm.items);
+  //       });
+  //     };
+  //     vm.getClass = function(index){
+  //      return index==0?'in':'';
+  //     }      
+
+  //     vm.tempUrl = svc.getTemplateUrl('accordion.html');
+  //   }
+  // });
+
+  // mtmDemo.component('slider', {
+  //   controllerAs: 'vm',
+  //   template: "<div ng-include='vm.tempUrl'></div>",
+  //   bindings: {
+  //     title: '@',
+  //     siteurl: '@'
+  //   },
+  //   controller: function (svc) {
+  //     var vm = this;
+     
+  //     vm.$onInit = function () {
+  //       var url = '/sites/demo/';
+  //       console.log("Site is " + vm.siteurl);
+  //       if(vm.siteurl){url =vm.siteurl;}
+  //       svc.getAllItemsFromSite("Slider", url).then(function (results) {
+  //         vm.items = results;          
+  //         console.log(vm.items);
+  //       });
+  //     };
+  //     vm.getClass = function(index){
+  //      return index==0?'active':'';
+  //     }      
+
+  //     vm.tempUrl = svc.getTemplateUrl('slider.html');
+  //   }
+  // });
+
+  // mtmDemo.component('tableItems', {
+  //   controllerAs: 'vm',
+  //   template: "<div ng-include='vm.tempUrl'></div>",
+  //   bindings: {
+  //     title: '@'
+  //   },
+  //   controller: function (svc) {
+  //     var vm = this;
+  //     vm.search = '';
+  //     vm.$onInit = function () {
+        
+  //       svc.getAllItems("Board").then(function (results) {
+  //         vm.items = results;          
+  //         console.log(vm.items);
+  //       });
+  //     };
+  //     vm.getClass = function(index){
+  //      return index==0?'in':'';
+  //     }      
+
+  //     vm.tempUrl = svc.getTemplateUrl('listItems.html');
+  //   }
+  // });
+
+
+
+})();
---
+++ app/app.ng.services.js
@@ -0,0 +1,109 @@
+
+(function () {
+  'use strict';
+
+  mtmDemo.factory('svc', ['$http', '$q', function ($http, $q) {
+    var _webAbsoluteUrl = _spPageContextInfo.webAbsoluteUrl;
+   
+    //RequestHeader for get,post,update,delete
+    var requestHeader = {
+      getHeader: {
+        'headers': {
+          'accept': 'application/json;odata=verbose'
+        }
+      },
+      postHeader: {
+        'headers': {
+          "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+          'content-type': 'application/json;odata=verbose',
+          'accept': 'application/json;odata=verbose'
+        }
+      },
+      deleteHeader: {
+        'headers': {
+          "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+          'content-type': 'application/json;odata=verbose',
+          "IF-MATCH": "*"
+        }
+      },
+      updateHeader: {
+        'headers': {
+          "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+          'content-type': 'application/json;odata=verbose',
+          'accept': 'application/json;odata=verbose',
+          "IF-MATCH": "*",
+          "X-HTTP-Method": "MERGE"
+        }
+      }
+    };
+    /**SharePoint CRUD Operations Starts here**/
+    function _getLisItems(urlValue) {
+      return $http.get(urlValue, requestHeader.getHeader)
+        .then(function (response) {
+          return response.data.d.results; //TODO: View in Console
+        });
+    };
+
+    function _getListItem(endpoint) {
+      return $http.get(endpoint, requestHeader.getHeader).then(function (response) {
+        return response.data.d;//Todo: View in browser
+      });
+    };
+
+    function _CreateListData(urlValue, data) {
+      return $http.post(urlValue, data, requestHeader.postHeader)
+        .then(function (response) {
+          return response;//'Created from Custom Service...';
+        });
+    };
+
+    function _deleteListData(urlValue) {
+      return $http.delete(urlValue, requestHeader.deleteHeader)
+        .then(function (response) {
+          return response;
+        });
+    };
+
+    function _updateListData(urlValue, data) {
+      return $http.post(urlValue, data, requestHeader.updateHeader)
+        .then(function (response) {
+          return response;
+        });
+    };
+
+    /**SharePoint CRUD Operation Ends here**/
+
+
+
+    return {
+      getTemplateUrl: function(temlateName){
+        return '/sites/branding/Style Library/app/templates/' + temlateName;
+      },
+      getItems: function(listName, filter){
+        if(!filter)filter = '';
+        var url = _webAbsoluteUrl + "/_api/web/lists/getbytitle('"+listName+"')/items" + filter;
+        return _getLisItems(url).then(function(response){
+          //console.log(response);
+          return response;
+        });
+      },
+      getAllItems: function(listName){        
+        var url = _webAbsoluteUrl + "/_api/web/lists/getbytitle('"+listName+"')/items";
+        return _getLisItems(url).then(function(response){
+          //console.log(response);
+          return response;
+        });
+      },
+      getAllItemsFromSite: function(listName,siteurl){        
+        var url = siteurl.replace(/\/$/, "") + "/_api/web/lists/getbytitle('"+listName+"')/items";
+        return _getLisItems(url).then(function(response){
+          //console.log(response);
+          return response;
+        });
+      }
+
+    };//End of Service Return
+
+  }]);//ends svc function
+
+})();//main anonymous function
